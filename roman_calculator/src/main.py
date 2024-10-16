086598131de073900490ac1165e3a0944229401c
DarienAllison1 <124842514+DarienAllison1@users.noreply.github.com>
2024-10-16 15:18:42 -0400

    Update main.py

--- roman_calculator/src/main.py
+++ roman_calculator/src/main.py
@@ -1,6 +1,8 @@
 from calculator import calculate
+import sys
 
 if __name__ == '__main__':
-    expression = input("Enter a Roman numeral expression: ")
+    expression = " ".join(sys.argv[1:])
+    #expression = input("Enter a Roman numeral expression: ")
     result = calculate(expression)
     print(result)
